c89108f417cd435b15c72a3cf6620854a7a56e51
Laddge <dev.laddge@gmail.com>
2021-05-25 17:22:45 +0900

    [feat] add footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+    <footer class="footer">
+        <small>
+        Designed by <a href="https://github.com/laddge/HatoStrap">Laddge</a>.<br>
+        Copyright &copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a> All Rights Reserved.
+        </small>
+    </footer>
+</body>
+
+</html>

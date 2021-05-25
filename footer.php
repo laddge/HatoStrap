c172767b76e9348c13202cdce900bffd0691a53d
Laddge <dev.laddge@gmail.com>
2021-05-26 07:20:19 +0900

    [feat] フッターのスタイリング

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-    <footer class="footer">
+    <footer class="footer text-light text-center bg-dark">
         <small>
         Designed by <a href="https://github.com/laddge/HatoStrap">Laddge</a>.<br>
         Copyright &copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a> All Rights Reserved.

fd95c946da00e86a3f6f77334873db8f9686c0a7
Laddge <dev.laddge@gmail.com>
2021-05-26 08:03:34 +0900

    [feat] ヘッダーにサイトアイコンを表示

--- header.php
+++ header.php
@@ -10,8 +10,9 @@
 <body <?php body_class(); ?>>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="<?php echo home_url(); ?>">
-                <?php bloginfo('name'); ?>
+            <a class="navbar-brand d-flex align-items-center" href="<?php echo home_url(); ?>">
+                <img src="<?php echo get_site_icon_url(); ?>" alt="" height="30">
+                <span class="pl-2"><?php bloginfo('name'); ?></span>
             </a>
         </nav>
     </header>

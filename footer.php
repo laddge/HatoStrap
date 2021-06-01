e3e45ab311d291828e92f9d9ef4fb0a086a79fd3
Laddge <dev.laddge@gmail.com>
2021-06-01 21:22:53 +0900

    [fix] フッター周りのマージン・パディングを修整

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-    <footer class="footer text-light text-center bg-dark">
+    <footer class="footer text-light text-center bg-dark mt-2 py-2">
         <small>
         Designed by <a href="https://github.com/laddge/HatoStrap" class="text-white">Laddge</a>.<br>
         Copyright &copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>" class="text-white"><?php bloginfo('name'); ?></a> All Rights Reserved.

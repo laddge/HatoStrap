23bb4d8974a63e66001322e1b1addbea5e643687
Laddge <dev.laddge@gmail.com>
2021-09-20 16:36:05 +0900

    [fix] Sticky FooterをBootstrap化した

--- footer.php
+++ footer.php
@@ -1,11 +1,13 @@
+            </div>
+        </div>
+        <footer class="footer text-light text-center bg-dark mt-auto py-2">
+            <small>
+            Designed by <a href="https://github.com/laddge/HatoStrap" class="text-white">Laddge</a>.<br>
+            Copyright &copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>" class="text-white"><?php bloginfo('name'); ?></a> All Rights Reserved.
+            </small>
+        </footer>
+        <?php wp_footer(); ?>
     </div>
-    <footer class="footer text-light text-center bg-dark mt-2 py-2">
-        <small>
-        Designed by <a href="https://github.com/laddge/HatoStrap" class="text-white">Laddge</a>.<br>
-        Copyright &copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>" class="text-white"><?php bloginfo('name'); ?></a> All Rights Reserved.
-        </small>
-    </footer>
-    <?php wp_footer(); ?>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="h-100">
 
 <head>
     <meta charset="UTF-8">
@@ -7,46 +7,48 @@
     <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand d-flex align-items-center" href="<?php echo home_url(); ?>">
-                <img src="<?php echo get_site_icon_url(); ?>" alt="" height="30">
-                <span class="pl-2"><?php bloginfo('name'); ?></span>
-            </a>
-            <button class="navbar-toggler btn" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <?php
-                    $menu_name = 'header-menu';
+<body <?php body_class('h-100');?>>
+    <div class="d-flex flex-column h-100">
+        <div class="flex-row">
+            <header>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand d-flex align-items-center" href="<?php echo home_url(); ?>">
+                        <img src="<?php echo get_site_icon_url(); ?>" alt="" height="30">
+                        <span class="pl-2"><?php bloginfo('name'); ?></span>
+                    </a>
+                    <button class="navbar-toggler btn" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <?php
+                            $menu_name = 'header-menu';
 
-                    if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
-                        $menu = wp_get_nav_menu_object($locations[$menu_name]);
+                            if (($locations = get_nav_menu_locations()) && isset($locations[$menu_name])) {
+                                $menu = wp_get_nav_menu_object($locations[$menu_name]);
 
-                        $menu_items = wp_get_nav_menu_items($menu->term_id);
+                                $menu_items = wp_get_nav_menu_items($menu->term_id);
 
-                        $menu_list = '';
+                                $menu_list = '';
 
-                        foreach ((array) $menu_items as $key => $menu_item) {
-                            $title = $menu_item->title;
-                            $url = $menu_item->url;
-                            $serverUrl = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-                            if ($url == $serverUrl || $url . '/' == $serverUrl) {
-                                $active = ' active';
+                                foreach ((array) $menu_items as $key => $menu_item) {
+                                    $title = $menu_item->title;
+                                    $url = $menu_item->url;
+                                    $serverUrl = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+                                    if ($url == $serverUrl || $url . '/' == $serverUrl) {
+                                        $active = ' active';
+                                    } else {
+                                        $active = '';
+                                    }
+                                    $menu_list .= '<li class="nav-item' . $active . '"><a class="nav-link" href="' . $url . '">' . $title . '</a></li>';
+                                }
                             } else {
-                                $active = '';
+                                $menu_list = '<ul><li>Menu "' . $menu_name . '" not defined.</li></ul>';
                             }
-                            $menu_list .= '<li class="nav-item' . $active . '"><a class="nav-link" href="' . $url . '">' . $title . '</a></li>';
-                        }
-                    } else {
-                        $menu_list = '<ul><li>Menu "' . $menu_name . '" not defined.</li></ul>';
-                    }
-                    echo $menu_list;
-                    ?>
-                </ul>
-            </div>
-        </nav>
-    </header>
-    <div class="main-contents">
+                            echo $menu_list;
+                            ?>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <div>

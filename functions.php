bd1aeabdc73d3b3c6e2594edc960d2495a3ef6d6
Laddge <dev.laddge@gmail.com>
2021-09-20 16:48:21 +0900

    [feat] Bootstrapのバージョンを5.1.1にアップデート

--- functions.php
+++ functions.php
@@ -1,15 +1,13 @@
 <?php
 
 function mybootstrap_enqueue_styles() {
-    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css');
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css');
     $dependencies = array('bootstrap');
     wp_enqueue_style('mybootstrap-style', get_stylesheet_uri(), $dependencies);
 }
 
 function mybootstrap_enqueue_scripts() {
-    wp_register_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js');
-    $dependencies = array('jquery', 'popper');
-    wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js', $dependencies, '4.2.1', true);
+    wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js', [], '5.1.1');
 }
 
 add_action( 'wp_enqueue_scripts', 'mybootstrap_enqueue_styles' );
